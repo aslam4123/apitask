19de416158328b73f87f2b6389a94192a6164129
aslam <thoppilaslam0987654321@gmail.com>
2025-01-06 09:10:27 +0530

    file added

--- reactapi/src/Components/TaskList.jsx
+++ reactapi/src/Components/TaskList.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 
 function TaskList(){
-    const [data,setdata]=useState([])
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        axios.get("http://127.0.0.1:8000/api/task/").then(res=>{
+            console.log(res.data);
+            setData(res.data)
+        }).catch(error=>console.log(error.message))
+    },[])
     return(
         <>
-        <table>
+        <div className="container">
+            
+        <table className="table">
             <thead>
             <tr>
                 <th>Task</th>
@@ -16,10 +26,13 @@ function TaskList(){
                 <tr key={index}>
                 <td>{value.title}</td>
                 <td>{value.dis}</td>
+                <td><button onClick={()=>{}}className="btn btn-outline-primary">Edit</button></td>
+                <td><button onClick={()=>{}}className="btn btn-outline-danger">Delete</button></td>                
                 </tr>
             ))}
             </tbody>
         </table>
+        </div>
         </>
     )
 }
