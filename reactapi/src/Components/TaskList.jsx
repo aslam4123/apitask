f1954e397b42309e462070e7f4043f389d7368cb
aslam <thoppilaslam0987654321@gmail.com>
2025-01-09 08:26:55 +0530

    new

--- reactapi/src/Components/TaskList.jsx
+++ reactapi/src/Components/TaskList.jsx
@@ -17,6 +17,13 @@ function TaskList(){
         setEditDtls(task)
 
     }
+    const Updatedtls = (id,task)=>{
+        setEditing(false)
+        axios.put(`http://127.0.0.1:8000/api/task/${id}/`,task).then(res=>{
+            setData(data.map((prv)=>prv.id===id? res.data : prv))
+        }).catch(error=>console.log(error.message)
+        )
+    }
     return(
         <>
         <div className="container">
@@ -39,22 +46,32 @@ function TaskList(){
             ))}
             </tbody>
         </table>
-        {editing ? <EditForm curTask={editDtls}/>:null}
+        {editing ? <EditForm curTask={editDtls} updatetask={Updatedtls}/>:null}
         </div>
         </>
     )
 }
 
-const EditForm=({curTask})=>{
+const EditForm=({curTask,updatetask})=>{
     console.log('EditForm',curTask);
     
     const [task,setTask] = useState(curTask)
 
+    const handleChange = (e) =>{
+        const {name,value} = e.target
+        setTask({...task,[name]:value})
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        updatetask(task.id,task)
+    }    
+
     return(
         <>
-            <form>
-                <input type="text" name="task" id="task" value={task.title}/>
-                <input type="text" name="dis" id="dis" value={task.dis}/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="task" id="task" value={task.title} onChange={handleChange}/>
+                <input type="text" name="dis" id="dis" value={task.dis} onChange={handleChange}/>
                 <input type="submit" value="Update" />
             </form>
         </>
